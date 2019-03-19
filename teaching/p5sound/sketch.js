15c658d3ebf67cf64ac4f305a69bf61d03388dfa
L A R K <lark.buckingham@sjsu.edu>
2019-03-19 09:36:29 -0700

    added osc exercise

---
+++ teaching/p5sound/sketch.js
@@ -0,0 +1,22 @@
+let osc;
+
+
+function setup() {
+  createCanvas(400, 400);
+  osc = new p5.Oscillator('square');
+}
+
+function draw() {
+  background(220);
+  osc.freq(60 + mouseX);
+  osc.amp(map(mouseY, 0, height, 0.2, 0));
+  // rect(50, 50, map(mouseX, 0, width, 0, 200), map(mouseY, 0, height, 0, 200));
+}
+
+function mousePressed() {
+  osc.start();
+}
+
+function mouseReleased() {
+  osc.stop();
+}
