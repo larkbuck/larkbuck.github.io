5c88bb0608d77c443ecdaaab9763a702466acc24
L A R K <lark.buckingham@sjsu.edu>
2019-03-21 09:51:51 -0700

    updates sound zip

--- teaching/sound/01_playSound/sketch.js
+++ teaching/sound/01_playSound/sketch.js
@@ -7,22 +7,12 @@
 // https://www.youtube.com/playlist?list=PLRqwX-V7Uu6aFcVjlDAkkGIixw70s7jpW
 
 
-let song;
 
-function preload() {
-  song = loadSound('../assets/music/astronomy-self-texture.mp3');
-}
 
 function setup() {
-  // song.play()
+
 }
 
 function draw() {
 
-}
-
-function mousePressed (){
-  song.setVolume(.3);
-  // song.play();
-  song.loop();
 }
