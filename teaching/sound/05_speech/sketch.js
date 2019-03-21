cedae9f15661d6b19926dfea81056afa25d50566
L A R K <lark.buckingham@sjsu.edu>
2019-03-21 09:48:41 -0700

    finished sound tuts added zips

--- teaching/sound/05_speech/sketch.js
+++ teaching/sound/05_speech/sketch.js
@@ -4,36 +4,10 @@
 // example source code: https://github.com/IDMNYU/p5.js-speech/blob/master/examples/01simple.html
 
 
-
-let talkButton;
-let voice;
-
-
 function setup() {
-  talkButton = select('#talk');
-  talkButton.mousePressed('startVoice');
 
-  voice = new p5.Speech();
-  voice.listVoices();
 }
 
 function draw() {
 
 }
-
-function startVoice() {
-  voice.speak('hello');
-  voice.listVoices();
-}
-//
-// let talkButton;
-//
-//
-// function setup() {
-//   talkButton = select('#talk');
-//   talkButton.mousePressed('startVoice');
-// }
-//
-// function draw() {
-//
-// }
